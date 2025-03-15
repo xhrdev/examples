e7d89f25f80f998b25fe094d8c40e112976ef690
John Skilbeck <john.skilbeck@gmail.com>
2025-03-15 01:53:22 -0700

    fix(integration): init airbnb

--- src/airbnb/auth.ts
+++ src/airbnb/auth.ts
@@ -24,8 +24,9 @@ dotenv.config();
 const xhrApiKey = process.env.XHR_API_KEY;
 if (!xhrApiKey) throw new Error('set XHR_API_KEY in .env file');
 
-const email = process.env.APOLLO_EMAIL;
-const password = process.env.APOLLO_PASSWORD;
+const email = process.env.AIRBNB_EMAIL;
+const password = process.env.AIRBNB_PASSWORD;
+console.log({ email, password });
 if (!email || !password) throw new Error('set email and password in .env file');
 
 const HttpsProxyCookieAgent = createCookieAgent(HttpsProxyAgent);
@@ -34,47 +35,12 @@ const httpsProxyCookieAgent = new HttpsProxyCookieAgent(proxyUrl, {
   cookies: { jar },
 });
 
-const cacheKey = Date.now();
-const timezoneOffset = 480;
-
 await axios.request({
   headers: {
     'x-xhr-api-key': xhrApiKey,
   },
   httpsAgent: httpsProxyCookieAgent,
-  url: 'https://app.apollo.io/',
+  url: 'https://www.airbnb.com/login',
 });
 
 if (!jar.serializeSync()?.cookies.length) throw new Error('no cookies');
-
-await axios.request({
-  headers: {
-    accept: '*/*',
-    'content-type': 'application/json',
-    'x-xhr-api-key': xhrApiKey,
-  },
-  httpsAgent: httpsProxyCookieAgent,
-  url: `https://app.apollo.io/api/v1/auth/check?timezone_offset=${timezoneOffset}&current_finder_view_id=&cacheKey=${cacheKey}`,
-});
-
-const csrf = getCsrfCookieFromJar({ cookieName: 'X-CSRF-TOKEN', jar })?.value;
-if (!csrf) throw new Error('no csrf');
-
-const { data: result } = await axios.request({
-  data: JSON.stringify({
-    cacheKey,
-    email,
-    password,
-    timezone_offset: timezoneOffset,
-  }),
-  headers: {
-    'content-type': 'application/json',
-    'x-csrf-token': csrf,
-    'x-xhr-api-key': xhrApiKey,
-  },
-  httpsAgent: httpsProxyCookieAgent,
-  method: 'POST',
-  url: 'https://app.apollo.io/api/v1/auth/login',
-});
-
-console.log({ cookies: stringifyCookiesFromJar({ jar }), csrf });
