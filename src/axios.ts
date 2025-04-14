6e3ff42e50e2bf278d8999d64419ae40f9a3f08c
John Skilbeck <john.skilbeck@gmail.com>
2025-04-13 22:39:57 -0700

    docs: add axios

---
+++ src/axios.ts
@@ -0,0 +1,41 @@
+/**
+ * run this script:
+
+npm run tsx src/axios.ts
+
+ */
+import axios from 'axios';
+import { CookieJar } from 'tough-cookie';
+import { wrapper } from 'axios-cookiejar-support';
+import { HttpsProxyAgent } from 'https-proxy-agent';
+import { createCookieAgent } from 'http-cookie-agent/http';
+import * as dotenv from 'dotenv';
+
+import { proxyUrl } from '@src/utils';
+
+wrapper(axios);
+dotenv.config();
+
+const xhrApiKey = process.env.XHR_API_KEY;
+if (!xhrApiKey) throw new Error('set XHR_API_KEY in .env file');
+
+const HttpsProxyCookieAgent = createCookieAgent(HttpsProxyAgent);
+const jar = new CookieJar();
+const httpsProxyCookieAgent = new HttpsProxyCookieAgent(proxyUrl, {
+  cookies: { jar },
+});
+
+const response = await axios.request({
+  headers: {
+    'x-xhr-api-key': xhrApiKey,
+  },
+  httpsAgent: httpsProxyCookieAgent,
+  url: 'https://core.cro.ie',
+});
+
+if (!jar.serializeSync()?.cookies.length) throw new Error('no cookies');
+
+console.log({
+  cookies: jar.serializeSync()?.cookies,
+  response: response.data,
+});
